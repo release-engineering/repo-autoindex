254e0f7cd93862f9379aee813c0ff20ef620f162
Rohan McGovern <rmcgover@redhat.com>
2022-08-08 09:50:52 +1000

    Add some documentation

---
+++ docs/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "repo-autoindex"
+copyright = "2022, Red Hat"
+author = "Rohan McGovern <rmcgover@redhat.com>"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.napoleon",
+    "sphinxarg.ext",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+html_theme_options = {
+    # Disable showing the sidebar. Defaults to 'false'
+    "nosidebar": True,
+}
+
+
+# -- Options for intersphinx extension ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
--- repo_autoindex/_impl/cmd.py
+++ repo_autoindex/_impl/cmd.py
@@ -2,10 +2,11 @@ import argparse
 import asyncio
 import logging
 import os
+from typing import Any
 
 from repo_autoindex import autoindex
 
-LOG = logging.getLogger("autoindex")
+LOG = logging.getLogger("repo-autoindex")
 
 
 async def dump_autoindices(args: argparse.Namespace) -> None:
@@ -18,12 +19,28 @@ async def dump_autoindices(args: argparse.Namespace) -> None:
         LOG.info("Wrote %s", output)
 
 
+def argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Generate indexes for a repository accessed via HTTP(S)"
+    )
+    parser.add_argument("url", help="Base URL of repository to be indexed")
+    parser.add_argument(
+        "--index-filename",
+        metavar="FILENAME",
+        default="index.html",
+        help="Basename of output file(s)",
+    )
+    parser.add_argument("--debug", action="store_true", help="Enable verbose logging")
+    return parser
+
+
 def entrypoint() -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url")
-    parser.add_argument("--index-filename", default="index.html")
-    parser.add_argument("--debug", action="store_true")
+    parser = argparser()
     p = parser.parse_args()
 
-    logging.basicConfig(level=logging.DEBUG if p.debug else logging.INFO)
+    kwargs: dict[str, Any] = {"level": logging.DEBUG if p.debug else logging.INFO}
+    if not p.debug:
+        kwargs["format"] = "%(message)s"
+
+    logging.basicConfig(**kwargs)
     asyncio.run(dump_autoindices(p))
