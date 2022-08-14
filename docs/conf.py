f06b623af46408c4bcd41a89d6307e4bac2aa799
Rohan McGovern <rmcgover@redhat.com>
2022-08-15 09:34:53 +1000

    Release a version 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "repo-autoindex"
 copyright = "2022, Red Hat"
 author = "Rohan McGovern <rmcgover@redhat.com>"
-release = "0.1.0"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
